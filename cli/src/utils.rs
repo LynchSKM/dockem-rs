f0b94dd401ce5dde8ec3d79161cadd6e550159f4
Lynch Mwaniki <lsking16@gmail.com>
2025-02-14 00:24:59 +0200

    Add a file exists util function

--- cli/src/utils.rs
+++ cli/src/utils.rs
@@ -1,4 +1,5 @@
-mod hash_watch_directories;
-mod hash_watch_files;
+pub mod file_exists;
+pub mod hash_watch_directories;
+pub mod hash_watch_files;
 pub mod os_open;
 pub mod parse_version_file_json;
