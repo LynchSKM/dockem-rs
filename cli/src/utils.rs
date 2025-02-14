533777f71e68fecf285af6c05dd9fe68d13c5c50
Lynch Mwaniki <lsking16@gmail.com>
2025-02-14 03:35:44 +0200

    Create an assert file exists util function

--- cli/src/utils.rs
+++ cli/src/utils.rs
@@ -1,10 +1,17 @@
 mod directory_exists;
 mod extract_version;
+
+mod assert_file_exists;
+pub use assert_file_exists::assert_file_exists;
 mod file_exists;
+mod macros;
 pub use file_exists::*;
+
 mod hash_watch_directories;
 mod hash_watch_files;
 mod os_open;
 pub use os_open::*;
+
 mod parse_version_file_json;
+
 pub use parse_version_file_json::*;
